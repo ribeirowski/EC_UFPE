901be70d2b5fd733a66223beb2576ff60846c699
ribeirowski <enioribeiro9@hotmail.com>
2022-08-09 00:32:45 -0300

    Update eyeDetection.py

--- PET_Visao_Computacional/Programas_Oficial/haarcascades/eyeDetection.py
+++ PET_Visao_Computacional/Programas_Oficial/haarcascades/eyeDetection.py
@@ -1,22 +1,7 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
-def detectAndDisplay(frame):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
-    #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-        for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    cv.imshow('Capture - Face detection', frame)
+
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
 parser.add_argument('--face_cascade', help='Path to face cascade.', default='haarcascade_frontalface_alt.xml')
 parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='haarcascade_eye_tree_eyeglasses.xml')
@@ -26,24 +11,66 @@ face_cascade_name = args.face_cascade
 eyes_cascade_name = args.eyes_cascade
 face_cascade = cv.CascadeClassifier()
 eyes_cascade = cv.CascadeClassifier()
-#-- 1. Load the cascades
+
 if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
     print('--(!)Error loading face cascade')
     exit(0)
 if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
     print('--(!)Error loading eyes cascade')
     exit(0)
-camera_device = args.camera
-#-- 2. Read the video stream
+
 cap = cv.VideoCapture(0)
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
-while True:
+
+print("[1] Face Detection")
+print("[2] Eye Detection")
+print("[3] Both face and eye Detection")
+print("[0] Quit\n")
+escolha = int(input("Selecione uma opção: "))
+
+while cap.isOpened():
     ret, frame = cap.read()
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)
+    faces = face_cascade.detectMultiScale(frame_gray)
+
+    if escolha == 1:
+        for (x, y , w ,h) in faces:
+            cv.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 3)
+            roi_gray = frame_gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+        # Display the output
+        cv.imshow('img', cv.flip(frame, 1))
+        if cv.waitKey(10) == 27:
+            break
+
+    if escolha == 2:
+        for (x, y , w ,h) in faces:
+            #cv.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 3)
+            roi_gray = frame_gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eyes_cascade.detectMultiScale(roi_gray)
+            for (ex, ey ,ew, eh) in eyes:
+                cv.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 3)
+        # Display the output
+        cv.imshow('img', cv.flip(frame, 1))
+        if cv.waitKey(10) == 27:
+            break
+
+    if escolha == 3:
+        for (x, y , w ,h) in faces:
+            cv.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 3)
+            roi_gray = frame_gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eyes_cascade.detectMultiScale(roi_gray)
+            for (ex, ey ,ew, eh) in eyes:
+                cv.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 3)
+
+        # Display the output
+        cv.imshow('img', cv.flip(frame, 1))
+        if cv.waitKey(10) == 27:
+            break
+
+    if escolha == 0:
         break
-    detectAndDisplay(frame)
-    if cv.waitKey(10) == 27:
-        break
+
+cap.release()
