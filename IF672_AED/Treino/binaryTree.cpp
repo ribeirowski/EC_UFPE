40a53ef105eaa3d67cd8656efd9cb7cdb32e91a0
ribeirowski <enioribeiro9@hotmail.com>
2022-08-22 23:07:39 -0300

    update

---
+++ IF672_AED/Treino/binaryTree.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+typedef struct binaryTree{
+    Node *root;
+} binaryTree;
+
+int main(){
+
+    return 0;
+}
